d15fcedfe263c2b327fe3f821f458fbdd4643dd0
ste9jpg <stejpg9@gmail.com>
2023-03-29 12:23:08 -0500

    adding event listener

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+  var entryb = document.getELementById('entrybu');
+  
+  entryb.addEventListener('alert', myEventFunction();
+  
+  entryb.addEventListener('alert', function(){
+	  console.log('Stephanie H: This is a test');
+  }
 
 
 
