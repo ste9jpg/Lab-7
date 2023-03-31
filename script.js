7775980b039898e66ec78025f2e486b98bde59ff
ste9jpg <stejpg9@gmail.com>
2023-03-30 21:06:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
-  var entryb = document.getELementById('entrybu');
+  var entryb = document.getELementById('alert-me');
   
   entryb.addEventListener('alert', myEventFunction();
   
   entryb.addEventListener('alert', function(){
 	  console.log('Stephanie H: This is a test');
-  }
-
+  });
+}
 
 
 
