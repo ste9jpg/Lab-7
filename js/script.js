4f7b4fb10c75e85ea9e465b3cf1da3dba638f94b
ste9jpg <83249446+ste9jpg@users.noreply.github.com>
2023-04-04 12:24:45 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ function init(){
     var textbox = document.getElementById('entryinput').value;
     alert('Stephanie H: ' + textbox);
     document.getElementById('textoutput').innerHTML = textbox;
-    });
+  });
+  button.addEventListener('click', showMeText);
   }
 
 window.addEventListener('load', init);
